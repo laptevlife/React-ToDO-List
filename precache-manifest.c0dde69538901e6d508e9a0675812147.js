a0a8451d56b55c91b67d052362e28187ff9b6ece
xxx <56048517+laptevlife@users.noreply.github.com>
2020-10-12 16:03:04 +0400

    Updates

---
+++ precache-manifest.c0dde69538901e6d508e9a0675812147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8394128d5ec4fd64187d2b77de726884",
+    "url": "/React-ToDO-List/index.html"
+  },
+  {
+    "revision": "598bd40deef5cddbe4ff",
+    "url": "/React-ToDO-List/static/css/main.e67cd3ab.chunk.css"
+  },
+  {
+    "revision": "07d685b33c15101fc127",
+    "url": "/React-ToDO-List/static/js/2.bfb44821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDO-List/static/js/2.bfb44821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598bd40deef5cddbe4ff",
+    "url": "/React-ToDO-List/static/js/main.018dcd61.chunk.js"
+  },
+  {
+    "revision": "a52d3c61d0ccd3860907",
+    "url": "/React-ToDO-List/static/js/runtime-main.a2c9702f.js"
+  }
+]);
